df4a8bcd652bebf9b82fa844dec33cfb14c14acc
kedaranthambekar <20091631@mail.wit.ie>
2023-11-09 16:20:04 +0000

    Confirm signup resource.

--- lib/auth-app-stack.ts
+++ lib/auth-app-stack.ts
@@ -43,6 +43,14 @@ export class AuthAppStack extends cdk.Stack {
       "SignupFn",
       'signup.ts'
     );
+      // NEW
+      this.addAuthRoute(
+        "confirm_signup",
+        "POST",
+        "ConfirmFn",
+        "confirm-signup.ts"
+      );
+    
 
 
   }
